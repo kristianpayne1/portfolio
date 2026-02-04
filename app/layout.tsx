3b85569cc47e773f7286e291b671878296928062
kristianpayne1 <kristianpayne1@gmail.com>
2026-02-04 18:19:15 +0000

    Run prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,70 +1,70 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
-import { baseUrl } from './sitemap'
+import "./global.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Navbar } from "./components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Footer from "./components/footer";
+import { baseUrl } from "./sitemap";
 
 import "@radix-ui/themes/styles.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: 'Kristian Payne | Home',
-    template: 'Kristian Payne | %s',
-  },
-  description: 'This is my portfolio.',
-  openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
-    url: baseUrl,
-    siteName: 'My Portfolio',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+    metadataBase: new URL(baseUrl),
+    title: {
+        default: "Kristian Payne | Home",
+        template: "Kristian Payne | %s",
     },
-  },
-}
+    description: "This is my portfolio.",
+    openGraph: {
+        title: "My Portfolio",
+        description: "This is my portfolio.",
+        url: baseUrl,
+        siteName: "My Portfolio",
+        locale: "en_US",
+        type: "website",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
+};
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html
-      lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <Theme>
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
-        </main>         
-        </Theme>
-      </body>
-    </html>
-  )
+    return (
+        <html
+            lang="en"
+            className={cx(
+                "text-black bg-white dark:text-white dark:bg-black",
+                GeistSans.variable,
+                GeistMono.variable
+            )}
+        >
+            <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+                <Theme>
+                    <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+                        <Navbar />
+                        {children}
+                        <Footer />
+                        <Analytics />
+                        <SpeedInsights />
+                    </main>
+                </Theme>
+            </body>
+        </html>
+    );
 }
