caf4290b23b93928be4e9dc6a94cd7c5a2cde06c
kristianpayne1 <kristianpayne1@gmail.com>
2026-02-04 18:26:31 +0000

    Uninstall geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import "./global.css";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
+import { Theme } from "@radix-ui/themes";
 
 import "@radix-ui/themes/styles.css";
 
@@ -38,23 +37,14 @@ export const metadata: Metadata = {
     },
 };
 
-const cx = (...classes) => classes.filter(Boolean).join(" ");
-
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <html
-            lang="en"
-            className={cx(
-                "text-black bg-white dark:text-white dark:bg-black",
-                GeistSans.variable,
-                GeistMono.variable
-            )}
-        >
-            <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+        <html lang="en">
+            <body>
                 <Theme>
                     <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
                         <Navbar />
