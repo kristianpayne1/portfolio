38c41c90fd98b8650e371137b27fd89e4716e89d
kristianpayne1 <kristianpayne1@gmail.com>
2026-04-07 22:07:49 +0100

    Update footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,23 +1,8 @@
-function ArrowIcon() {
-    return (
-        <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-                fill="currentColor"
-            />
-        </svg>
-    );
-}
+import { FaRss, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <footer className="mt-16 flex justify-between">
+        <footer className="mt-16 mb-4 flex justify-between">
             <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
                 <li>
                     <a
@@ -26,7 +11,7 @@ export default function Footer() {
                         target="_blank"
                         href="/rss"
                     >
-                        <ArrowIcon />
+                        <FaRss size={14} />
                         <p className="ml-2 h-7">rss</p>
                     </a>
                 </li>
@@ -35,9 +20,9 @@ export default function Footer() {
                         className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
                         rel="noopener noreferrer"
                         target="_blank"
-                        href="https://github.com/vercel/next.js"
+                        href="https://github.com/kristianpayne1"
                     >
-                        <ArrowIcon />
+                        <FaGithub size={14} />
                         <p className="ml-2 h-7">github</p>
                     </a>
                 </li>
@@ -46,10 +31,10 @@ export default function Footer() {
                         className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
                         rel="noopener noreferrer"
                         target="_blank"
-                        href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+                        href="https://www.linkedin.com/in/kristian-payne-015698166/"
                     >
-                        <ArrowIcon />
-                        <p className="ml-2 h-7">view source</p>
+                        <FaLinkedin size={14} />
+                        <p className="ml-2 h-7">linkedin</p>
                     </a>
                 </li>
             </ul>
